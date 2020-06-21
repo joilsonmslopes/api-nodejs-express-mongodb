cb414e41367e96c9d5406713dc49377b5c0e8e31
joilsonmslopes <joilsonmirandasl@hotmail.com>
2020-06-21 19:13:10 -0300

    Server criado no src/index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('Pronto')
+});
+
+app.listen(3000, () => {
+  console.log('Server is running ...')
+});
