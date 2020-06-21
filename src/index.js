fa31bcddcb292344468f9c0e1cac9a480f85408f
joilsonmslopes <joilsonmirandasl@hotmail.com>
2020-06-21 19:56:27 -0300

    rota post /register criada e editados database linha 3, model User linha 1 e src/index linha 9

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Pronto')
-});
+require('./controllers/authController')(app);
 
 app.listen(3000, () => {
   console.log('Server is running ...')
